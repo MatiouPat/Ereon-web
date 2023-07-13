435536859fca9f0eb81ef010f1f7eb788d933592
MatiouPat <pays.matthieuic@gmail.com>
2023-07-13 19:13:38 +0200

    Reorganize the editor component correctly

--- assets/components/editor.vue
+++ assets/components/editor.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="editor-wrapper" ref="editor">
+    <div class="editor-wrapper" @mousedown="onMouseDown" @mouseup="onMouseUp" @wheel="onWheel" @contextmenu="onContextMenu" ref="editor">
         <div class="editor" ref="map" :style="{ width: width * ratio + 'px', height: height * ratio + 'px' }">
             <picture>
                 <source type="image/webp" srcset="build/images/test2.webp">
@@ -21,83 +21,65 @@
             return {
                 width: 1200,
                 height: 1200,
-                ratio: 1
+                ratio: 1,
+                startX: 0,
+                startY: 0,
+                mapX: 0,
+                mapY: 0 
             }
         },
         methods: {
             /**
              * 
-             * @param {HTMLElement} element 
-             * @param {Function} cb 
+             * @param {*} e 
              */
-            scrollMap: function (element, cb) {
-                element.addEventListener('mousedown', onMouseDown);
-                let startX, startY;
-                let mapX, mapY;
-
-                /**
-                 * 
-                 * @param {PointerEvent} e 
-                 */
-                function onMouseDown(e)
-                {
-                    e.preventDefault();
-                    if(e.button === 2) {
-                        startX = e.screenX - element.offsetLeft;
-                        startY = e.screenY - element.offsetTop;
-                        mapX = element.scrollLeft;
-                        mapY = element.scrollTop;
-                        document.addEventListener('mousemove', onMouseMove);
-                        document.addEventListener('mouseup', onMouseUp, { once: true });
-                    }
-                }
-
-                /**
-                 * 
-                 * @param {PointerEvent} e 
-                 */
-                function onMouseUp(e)
-                {
-                    document.removeEventListener('mousemove', onMouseMove);
-                }
-
-                /**
-                 * 
-                 * @param {PointerEvent} e 
-                 */
-                function onMouseMove(e)
-                {
-                    cb([mapX - (e.screenX - element.offsetLeft - startX), mapY - (e.screenY - element.offsetTop - startY)]);
+            onMouseDown: function (e) {
+                e.preventDefault();
+                if(e.button === 2) {
+                    this.startX = e.screenX - this.$el.offsetLeft;
+                    this.startY = e.screenY - this.$el.offsetTop;
+                    this.mapX = this.$el.scrollLeft;
+                    this.mapY = this.$el.scrollTop;
+                    document.addEventListener('mousemove', this.onMouseMove);
                 }
             },
-            zoomMap: function (element) {
-                element.addEventListener('wheel', (e) => {
-                    e.preventDefault();
-                    if(e.ctrlKey == true){
-                        if (this.ratio - e.deltaY * 0.0005 <= 0.1) {
-                            this.ratio = 0.1;
-                        } else if (this.ratio - e.deltaY * 0.00045 >= 2.5) {
-                            this.ratio = 2.5;
-                        } else if (this.ratio >= 0.1 && this.ratio <= 2.5) {
-                            this.ratio = this.ratio - e.deltaY * 0.0005;
-                        }
-                    }
-                });
+            /**
+             * 
+             * @param {*} e 
+             */
+            onMouseMove: function (e) {
+                this.$el.scrollLeft = this.mapX - (e.screenX - this.$el.offsetLeft - this.startX)
+                this.$el.scrollTop = this.mapY - (e.screenY - this.$el.offsetTop - this.startY)
             },
-            rightclick: function (element) {
-                element.addEventListener('contextmenu', (e) => {
-                    e.preventDefault();
-                })
+            /**
+             * 
+             */
+            onMouseUp: function () {
+                document.removeEventListener('mousemove', this.onMouseMove);
+            },
+            /**
+             * 
+             * @param {*} e 
+             */
+            onWheel: function (e) {
+                e.preventDefault();
+                if(e.ctrlKey == true){
+                    if (this.ratio - e.deltaY * 0.0005 <= 0.1) {
+                        this.ratio = 0.1;
+                    } else if (this.ratio - e.deltaY * 0.00045 >= 2.5) {
+                        this.ratio = 2.5;
+                    } else if (this.ratio >= 0.1 && this.ratio <= 2.5) {
+                        this.ratio = this.ratio - e.deltaY * 0.0005;
+                    }
+                }
+            },
+            /**
+             * 
+             * @param {*} e 
+             */
+            onContextMenu: function (e) {
+                e.preventDefault();
             }
-        },
-        mounted: function () {
-            let editor = this.$refs.editor
-            this.scrollMap(editor, function (coordinate) {
-                editor.scrollLeft = coordinate[0]
-                editor.scrollTop = coordinate[1]
-            })
-            this.zoomMap(editor)
-            this.rightclick(editor)
         }
     }
 </script>
