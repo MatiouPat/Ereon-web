01549aa4f2702e0d6221e9d53f91e7d19218bcbd
MatiouPat <pays.matthieuic@gmail.com>
2023-07-16 22:00:24 +0200

    Uninstall sensio_framework_extra

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
