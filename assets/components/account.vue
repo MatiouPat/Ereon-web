197252ef341f41ec08337613a47a3e6a0e98155e
MatiouPat <pays.matthieuic@gmail.com>
2023-08-24 23:04:45 +0200

    Update user parameters display

--- assets/components/account.vue
+++ assets/components/account.vue
@@ -1,19 +1,28 @@
 <template>
-    <div v-if="isConnected" class="header">
-        <div class="header-title-box" @click="viewAccount">
-            <img class="account-picture" src="build/images/logo/icon_120.png" alt="Ereon" width="120" height="120">
-            <span class="account-username">{{ getUsername }}</span>
-        </div>
-        <ul class="account-actions" v-if="isVisible">
-            <li><a href="/logout" style="font-weight: 700; color: red;">Se déconnecter</a></li>
-        </ul>
-        <div class="connectedUsers" v-for="connectedUser in getConnectedUser" :key="connectedUser.id">
-            <div class="header-title-box" @click="viewAccount">
-                <img class="other-user-picture" src="build/images/logo/icon_120.png" alt="Ereon" width="80" height="80">
+    <header v-if="isConnected" class="header">
+        <nav class="navigation">
+            <ul>
+                <li title="Paramètres"><img src="build/images/icons/settings.svg" width="24" height="24" alt="Paramètres"></li>
+                <li title="Se déconnecter"><a href="/logout"><img src="build/images/icons/logout.svg" width="24" height="24" alt="Se déconnecter"></a></li>
+            </ul>
+        </nav>
+        <div class="connected-users" v-for="connectedUser in getConnectedUser" :key="connectedUser.id">
+            <div class="header-title-box">
+                <img class="account-picture" src="build/images/logo/icon_120.png" alt="Ereon" width="64" height="64">
                 <span class="account-username">{{ connectedUser.username }}</span>
             </div>
         </div>
-    </div>
+        <Teleport to="#content">
+            <div class="parameters-wrapper" v-if="onParameters">
+                <div class="parameters">
+                    <div class="parameters-header">
+                        <h2>Paramètres</h2>
+                        <img @click="onParameters = false" src="build/images/icons/close.svg" alt="Fermer">
+                    </div>
+                </div>
+            </div>
+        </Teleport>
+    </header>
     <div v-else class="worlds-page">
         <h1>Quel monde ?</h1>
         <div class="world-layout" v-if="!worlds.length">
@@ -47,15 +56,12 @@ import { World } from '../interfaces/world';
     export default defineComponent({
         data() {
             return {
-                /**
-                 * If the account context box is visible
-                 */
-                isVisible: false as boolean,
+                emitter: inject('emitter') as any,
                 /**
                  * If the world has been chosen and all related variables are updated (players, map, tokens, etc.)
                  */
                 isConnected: false as boolean,
-                emitter: inject('emitter') as any
+                onParameters: false as boolean
             }
         },
         props: [
@@ -83,13 +89,6 @@ import { World } from '../interfaces/world';
             ...mapActions('map', [
                 'setMap'
             ]),
-            /**
-             * Display account context box when clicked
-             */
-            viewAccount: function() {
-                this.isVisible = !this.isVisible
-                window.addEventListener('click', this.clickOutside)
-            },
             /**
              * Loading information after choosing a world
              * @param connection The connection between player and world
@@ -121,17 +120,10 @@ import { World } from '../interfaces/world';
                         this.setConnection(data)
                     }
                 }
-            },
-            /**
-             * Hide context box when clicked outside it
-             * @param {*} e 
-             */
-            clickOutside: function(e: any) {
-                if(!this.$el.contains(e.target)){
-                    window.removeEventListener('click', this.clickOutside)
-                    this.isVisible = false;
-                }
             }
+        },
+        mounted() {
+
         }
     })
 </script>
@@ -139,13 +131,30 @@ import { World } from '../interfaces/world';
 <style scoped>
 
     .header {
-        position: absolute;
-        bottom: 16px;
-        left: 16px;
-        z-index: 2;
+        min-width: 48px;
+        width: 48px;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+        z-index: 1;
+    }
+
+    .navigation {
+        height: 100%;
+    }
+
+    .navigation ul {
         display: flex;
-        justify-content: space-between;
+        flex-direction: column;
+        justify-content: flex-end;
+        align-items: center;
+        height: 100%;
         gap: 16px;
+        padding: 16px 0;
+
+    }
+
+    .navigation img {
+        display: block;
+        margin: 0 auto;
     }
 
     .header-title-box {
@@ -160,53 +169,65 @@ import { World } from '../interfaces/world';
         box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
     }
 
-    .account-picture {
+    .connected-users {
         position: absolute;
-        bottom: 16px;
-        left: calc(50% - 60px);
-        z-index: 1;
+        bottom: 8px;
+        left: 56px;
+        display: flex;
     }
 
     .account-username {
         z-index: 2;
         padding: 8px;
-        min-width: 120px;
+        min-width: 96px;
         background-color: rgba(0, 0, 0, 0.8);
         color: #FFFFFF;
         text-align: center;
+        font-size: .8rem;
     }
 
-    .account-actions {
-        position: absolute;
-        bottom: 60px;
-        left: 96px;
-        width: 160px;
-        z-index: 3;
-        background-color: #FFFFFF;
-    }
-
-    .other-user-picture {
+    .account-picture {
         position: absolute;
         bottom: 16px;
-        left: calc(50% - 40px);
+        left: calc(50% - 32px);
         z-index: 1;
-    }
-
-    .account-actions::after {
-        content: "";
-        position: absolute;
-        top: 8px;
-        left: -8px;
-        display : inline-block;
-        height : 0;
-        width : 0;
-        border-top : 8px solid transparent;
-        border-right : 8px solid #FFFFFF;
-        border-bottom : 8px solid transparent;
     }
 
     .account-actions li {
         padding: 8px;
+    }
+
+    .parameters-wrapper {
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 5;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100dvw;
+        height: 100dvh;
+        background-color: rgba(0, 0, 0, 0.8);
+    }
+
+    .parameters {
+        display: block;
+        min-width: 256px;
+        min-height: 256px;
+        background-color: #FFFFFF;
+    }
+
+    .parameters-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 8px;
+        border-bottom: solid 1px #565656;
+    }
+
+    h2 {
+        font-size: 1.5rem;
+        font-weight: 400;
     }
 
     .worlds-page {
--- assets/components/rightSideBox.vue
+++ assets/components/rightSideBox.vue
@@ -1,19 +1,19 @@
 <template>
     <ul class="right-side-box-actions">
-        <li @click="choose(0)">
-            <svg height="24px" viewBox="0 0 24 24" width="24px" :fill="pageIndex == 0 ? '#D68836' : '#565656'"><path d="M0 0h24v24H0zm21.02 19c0 1.1-.9 2-2 2h-14c-1.1 0-2-.9-2-2V5c0-1.1.9-2 2-2h14c1.1 0 2 .9 2 2v14z" fill="none"/><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM7.5 18c-.83 0-1.5-.67-1.5-1.5S6.67 15 7.5 15s1.5.67 1.5 1.5S8.33 18 7.5 18zm0-9C6.67 9 6 8.33 6 7.5S6.67 6 7.5 6 9 6.67 9 7.5 8.33 9 7.5 9zm4.5 4.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5 4.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm0-9c-.83 0-1.5-.67-1.5-1.5S15.67 6 16.5 6s1.5.67 1.5 1.5S17.33 9 16.5 9z"/></svg>
+        <li title="Chat" @click="choose(0)">
+            <svg height="24px" viewBox="0 0 24 24" width="24px" :fill="pageIndex == 0 ? '#D68836' : '#565656'"><path d="M0 0h24v24H0z" fill="none"/><path d="M21 3H3v18h18V3zM7.5 18c-.83 0-1.5-.67-1.5-1.5S6.67 15 7.5 15s1.5.67 1.5 1.5S8.33 18 7.5 18zm0-9C6.67 9 6 8.33 6 7.5S6.67 6 7.5 6 9 6.67 9 7.5 8.33 9 7.5 9zm4.5 4.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5 4.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm0-9c-.83 0-1.5-.67-1.5-1.5S15.67 6 16.5 6s1.5.67 1.5 1.5S17.33 9 16.5 9z"/></svg>
         </li>
-        <li @click="choose(1)">
-            <svg height="24px" viewBox="0 0 24 24" width="24px" :fill="pageIndex == 1 ? '#D68836' : '#565656'"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 5h-3v5.5c0 1.38-1.12 2.5-2.5 2.5S10 13.88 10 12.5s1.12-2.5 2.5-2.5c.57 0 1.08.19 1.5.51V5h4v2zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6z"/></svg>
+        <li title="Jukebox" v-if="isGameMaster" @click="choose(1)">
+            <svg height="24px" viewBox="0 0 24 24" width="24px" :fill="pageIndex == 1 ? '#D68836' : '#565656'"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M22 2H6v16h16V2zm-4 5h-3v5.5c0 1.38-1.12 2.5-2.5 2.5S10 13.88 10 12.5s1.12-2.5 2.5-2.5c.57 0 1.08.19 1.5.51V5h4v2zM4 6H2v16h16v-2H4V6z"/></svg>
         </li>
-        <li v-if="isGameMaster" @click="choose(2)">
-            <svg height="24px" viewBox="0 0 24 24" width="24px" :fill="pageIndex == 2 ? '#D68836' : '#565656'"><path d="M0 0h24v24H0z" fill="none"/><path d="M22 16V4c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2zm-11-4l2.03 2.71L16 11l4 5H8l3-4zM2 6v14c0 1.1.9 2 2 2h14v-2H4V6H2z"/></svg>
+        <li title="Assets" v-if="isGameMaster" @click="choose(2)">
+            <svg height="24px" viewBox="0 0 24 24" width="24px" :fill="pageIndex == 2 ? '#D68836' : '#565656'"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M22 18V2H6v16h16zm-11-6l2.03 2.71L16 11l4 5H8l3-4zM2 6v16h16v-2H4V6H2z"/></svg>
         </li>
     </ul>
     <div class="right-side-box-content">
         <dialog-view v-show="pageIndex == 0"></dialog-view>
         <music-view v-show="pageIndex == 1"></music-view>
-        <asset-view v-show="pageIndex == 2"></asset-view>
+        <asset-view v-if="isGameMaster" v-show="pageIndex == 2"></asset-view>
     </div>
 </template>
 
