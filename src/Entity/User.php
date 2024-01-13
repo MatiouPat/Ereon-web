850c6bacba8a9859f72f875a4645a5440c21ec26
MatiouPat <pays.matthieuic@gmail.com>
2024-01-13 17:42:11 +0100

    Prevent username from being blank

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -16,6 +16,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name:"`user`")]
@@ -38,6 +39,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[NotBlank]
     #[Groups(["user:read", "user:write", "world:read", "connection:read", "map:read", "dice:read"])]
     private ?string $username = null;
 
