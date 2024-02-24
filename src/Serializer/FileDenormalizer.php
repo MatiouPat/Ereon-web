893e8db94d19e8f6bd38a61dede61a3e2b29716f
MatiouPat <pays.matthieuic@gmail.com>
2024-02-24 18:05:27 +0100

    Upgrade to Symfony 7.0

--- src/Serializer/FileDenormalizer.php
+++ src/Serializer/FileDenormalizer.php
@@ -8,14 +8,21 @@ use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 class FileDenormalizer implements DenormalizerInterface
 {
 
-    public function denormalize($data, string $type, string $format = null, array $context = [])
+    public function denormalize($data, string $type, string $format = null, array $context = []): array
     {
         return $data;
     }
 
-    public function supportsDenormalization($data, string $type, string $format = null)
+    public function supportsDenormalization($data, string $type, string $format = null, array $context = []): bool
     {
         return $data instanceof File;
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            File::class => true,
+        ];
     }
 
 }
